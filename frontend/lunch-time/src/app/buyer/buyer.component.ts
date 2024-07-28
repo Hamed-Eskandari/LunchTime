897770700bcf8ec4d4a665799e893ef4d4f21510
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-07-28 18:57:42 +0200

    add 3 basic component with angular CLI

---
+++ frontend/lunch-time/src/app/buyer/buyer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buyer',
+  standalone: true,
+  imports: [],
+  templateUrl: './buyer.component.html',
+  styleUrl: './buyer.component.scss'
+})
+export class BuyerComponent {
+
+}
