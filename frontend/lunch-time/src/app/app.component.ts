afa3edc8a9553cceda111614f2126ac3f30c0b34
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-07-28 19:59:03 +0200

    add routing for 3 component regarding to angular 17+

--- frontend/lunch-time/src/app/app.component.ts
+++ frontend/lunch-time/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BuyerComponent } from './buyer/buyer.component';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,OrderComponent,BuyerComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
