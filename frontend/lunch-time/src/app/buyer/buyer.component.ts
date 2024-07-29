091394eb1b7c40cfdd321b1d8c3816c413f66e0c
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-07-30 00:11:23 +0200

    first code buyer component

--- frontend/lunch-time/src/app/buyer/buyer.component.ts
+++ frontend/lunch-time/src/app/buyer/buyer.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatStepperModule } from '@angular/material/stepper';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-buyer',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './buyer.component.html',
-  styleUrl: './buyer.component.scss'
+  styleUrls: ['./buyer.component.scss'],
+  standalone: true,
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatStepperModule,
+  ],
 })
-export class BuyerComponent {
+export class BuyerComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  orders: any[] = [];
+  displayedColumns: string[] = ['name', 'order', 'restaurant', 'price', 'time'];
 
+  constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({});
+  }
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      this.orders = JSON.parse(localStorage.getItem('orders') || '[]');
+    }
+  }
+
+  printOrders() {
+    window.print();
+  }
+
+  finalizeOrders() {
+    
+  }
 }
