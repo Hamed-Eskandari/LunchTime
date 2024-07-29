5a88d22db657dcde63ff98120bdea1f8df84e25b
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-07-29 23:03:35 +0200

    order component and home component first version

--- frontend/lunch-time/src/app/buyer/buyer.component.ts
+++ frontend/lunch-time/src/app/buyer/buyer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-buyer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './buyer.component.html',
   styleUrl: './buyer.component.scss'
 })
