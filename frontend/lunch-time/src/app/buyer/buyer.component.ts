924508d6c5a23a37f952a3969563167ca02e29b3
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-07-31 23:33:20 +0200

    download table buyer as csv

--- frontend/lunch-time/src/app/buyer/buyer.component.ts
+++ frontend/lunch-time/src/app/buyer/buyer.component.ts
@@ -89,5 +89,24 @@ export class BuyerComponent implements OnInit {
     }
     alert("Alle Daten in der Tabelle wurden gelöscht");
   }
+
+  downloadCSV() {
+    const csvData = this.convertToCSV(this.orders);
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'orders.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  convertToCSV(objArray: any[]) {
+    const header = Object.keys(objArray[0]).join(',');
+    const rows = objArray.map(obj => Object.values(obj).join(','));
+    return [header, ...rows].join('\r\n');
+  }
 }
 
