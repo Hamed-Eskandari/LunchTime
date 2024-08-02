1c1f29f309f77afcde4d99eee5083474cecf09f5
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-08-02 23:30:27 +0200

    add confirm and alert dialog component

---
+++ frontend/lunch-time/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss'],
+  imports: [CommonModule, MatButtonModule, MatDialogModule]
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+}
