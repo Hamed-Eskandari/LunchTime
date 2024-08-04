ae73540d4c5fd9e13125cd7aff4d84e86945c4af
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-08-04 12:06:08 +0200

    add error and change style login

--- frontend/lunch-time/src/app/login/login.component.ts
+++ frontend/lunch-time/src/app/login/login.component.ts
@@ -5,11 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule,MatButtonModule,MatIconModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
@@ -17,19 +29,34 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  private readonly validUsername: string = 'user';
-  private readonly validPassword: string = 'pass123';
+  private readonly validUsername: string = 'benutzer';
+  private readonly validPassword: string = 'passwort123';
+
+  // Error flag to show invalid credentials message
+  loginFailed: boolean = false;
 
   constructor(private router: Router) {}
 
+  isUsernameInvalid(): boolean {
+    return this.username.trim().length === 0;
+  }
+
+  isPasswordInvalid(): boolean {
+    return this.password.trim().length === 0;
+  }
+
   login() {
+    // Reset the error flag on each login attempt
+    this.loginFailed = false;
+
     if (
       this.username === this.validUsername &&
       this.password === this.validPassword
     ) {
       this.router.navigate(['/buyer']);
     } else {
-      alert('Der Benutzername oder das Passwort ist falsch.');
+      // Set the error flag if login fails
+      this.loginFailed = true;
     }
   }
 }
