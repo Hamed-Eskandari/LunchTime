558f5e70c287e28109e1ca4a3a0d5e8ea864647d
Hamed-Eskandari <hamed.es2308@gmail.com>
2024-08-10 11:22:12 +0200

    add logout button and change some styles

--- frontend/lunch-time/src/app/app.component.ts
+++ frontend/lunch-time/src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'lunch-time';
+  showLogoutButton = false;
+  
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      
+      const currentRoute = this.router.url;
+
+      
+      this.showLogoutButton =
+        currentRoute.includes('/home') ||
+        currentRoute.includes('/order') ||
+        currentRoute.includes('/buyer');
+    });
+
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
 }
